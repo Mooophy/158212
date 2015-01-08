3c59a0513cd78e41c9051074f612b7b5d5103b63
Mooophy <moophyalan@gmail.com>
2015-01-08 18:20:31 +1300

    unit test experiment using hello method

---
+++ as3/Rugby/UnitTest/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodHello()
+        {
+            var c = new Lib.Controller();
+            Assert.AreEqual("hello", c.hello());
+            
+        }
+    }
+}
