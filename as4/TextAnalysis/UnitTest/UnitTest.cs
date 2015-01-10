1f1f6ca1ed2ddb90941c31c7cbe04fa780ce7532
Mooophy <moophyalan@gmail.com>
2015-01-10 18:39:44 +1300

    add file structure for backend and test

---
+++ as4/TextAnalysis/UnitTest/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var c = new Lib.Controller();
+        }
+    }
+}
