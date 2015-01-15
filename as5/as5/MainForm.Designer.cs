6fef19eb46df47b59d29c0b0a057c69f19c67d1c
Mooophy <moophyalan@gmail.com>
2015-01-16 10:47:38 +1300

    experiment on grid

---
+++ as5/as5/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace as5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(138, 85);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(240, 150);
+            this.grid.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 333);
+            this.Controls.Add(this.grid);
+            this.Name = "MainForm";
+            this.Text = "Enrollment System";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+    }
+}
+
---
+++ as5/as5/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace as5
+{
+    public partial class MainForm : Form
+    {
+        Dictionary<int, string> _dic;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _dic = new Dictionary<int, string>();
+            _dic[1] = "test";
+            _dic[2] = "test2";
+
+            var arr = from row in _dic select new { Item = row.Key, Price = row.Value, Remark = 0 };
+            grid.DataSource = arr.ToArray();
+            grid.ForeColor = Color.Aqua;
+            grid.AutoSize = true;
+            grid.AllowUserToAddRows = true;
+            
+
+            grid.BackgroundColor = Color.Azure;
+            grid.RowHeaderMouseClick += delegate{ MessageBox.Show("test"); };
+            grid.ColumnHeaderMouseClick += delegate { MessageBox.Show("test2"); };
+        }
+    }
+}
