d632e183b65710524c3dc959535730b15dfdb07f
Mooophy <moophyalan@gmail.com>
2015-01-23 18:00:00 +1300

    add files

---
+++ as5/Lib/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Student
+    {
+        public readonly int Id;
+        public readonly string Name;
+        
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        
+        public override string ToString()
+        {
+            return "[" + this.Name + "](" + Id + ")";
+        }
+
+
+    }
+}
---
+++ as5/UnitTest/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestStudent
+    {
+        Student student = new Student(0, "moophy");
+        [TestMethod]
+        public void TestCtor()
+        {
+            var s = new Lib.Student(1,"alan");
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Assert.AreEqual("[moophy](0)", student.ToString());
+        }
+    }
+}
