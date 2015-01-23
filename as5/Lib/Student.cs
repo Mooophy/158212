0dd43e7b8bdafcaa112d5e6cfe58243876ce2370
Mooophy <moophyalan@gmail.com>
2015-01-23 18:52:36 +1300

    student done

--- as5/Lib/Student.cs
+++ as5/Lib/Student.cs
@@ -10,16 +10,20 @@ namespace Lib
     {
         public readonly int Id;
         public readonly string Name;
+        public readonly DateTime BirthDate;
+        public readonly string Address;
         
-        public Student(int id, string name)
+        public Student(int id, string name, DateTime date, string address)
         {
             Id = id;
             Name = name;
+            BirthDate = date;
+            Address = address;
         }
         
         public override string ToString()
         {
-            return "[" + this.Name + "](" + Id + ")";
+            return "[" + this.Name + "](" + Id + ")("+ BirthDate + ")(" + Address +")";
         }
 
 
--- as5/UnitTest/UnitTest.cs
+++ as5/UnitTest/UnitTest.cs
@@ -7,17 +7,27 @@ namespace UnitTest
     [TestClass]
     public class UnitTestStudent
     {
-        Student student = new Student(0, "moophy");
         [TestMethod]
         public void TestCtor()
         {
-            var s = new Lib.Student(1,"alan");
+            Student student = new Student(0, "moophy", new DateTime(), "some street");
+            Assert.AreEqual(0, student.Id);
+            Assert.AreEqual("moophy", student.Name);
+            Assert.AreEqual(new DateTime(), student.BirthDate);
+            Assert.AreEqual("some street", student.Address);
         }
 
         [TestMethod]
         public void TestToString()
         {
-            Assert.AreEqual("[moophy](0)", student.ToString());
+            Student student = new Student(0, "moophy", new DateTime(), "some street");
+            Assert.AreEqual("[moophy](0)(0001/1/1 0:00:00)(some street)",student.ToString());
         }
+    }
+
+    [TestClass]
+    public class UnitTestPaper
+    {
+ 
     }
 }
