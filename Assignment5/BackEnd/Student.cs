11fcd99be07cafb72197e1750639036fe7d592be
Mooophy <moophyalan@gmail.com>
2015-02-02 11:22:06 +1300

    add Student and tested

--- Assignment5/BackEnd/Student.cs
+++ Assignment5/BackEnd/Student.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BackEnd;
 
 namespace BackEnd
 {
-    public class Student
+    public class Student : IComparable
     {
+        /// <summary>
+        /// fields
+        /// </summary>
+        public readonly int Id;
+        public readonly string Name;
+        public readonly DateTime BirthDate;
+        public readonly string Address;
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            var other = obj as Student;
+            if (other != null)
+                return this.Id.CompareTo(other.Id);
+            else
+                throw new ArgumentException("object is not a Student");
+        }
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="date"></param>
+        /// <param name="address"></param>
+        public Student(int id, string name, DateTime date, string address)
+        {
+            Id = id;
+            Name = name;
+            BirthDate = date;
+            Address = address;
+        }
+        /// <summary>
+        /// Copy Ctor
+        /// </summary>
+        /// <param name="student"></param>
+        public Student(Student student)
+            : this(student.Id, student.Name, student.BirthDate, student.Address)
+        { }
+        /// <summary>
+        /// overrided ToString
+        /// </summary>
+        /// <returns>string</returns>
+        public override string ToString()
+        {
+            return Id + "," + Name + "," + BirthDate + "," + Address;
+        }
     }
 }
--- Assignment5/UnitTest/UnitTestForStudent.cs
+++ Assignment5/UnitTest/UnitTestForStudent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEnd;
 
 namespace UnitTest
 {
@@ -7,8 +8,13 @@ namespace UnitTest
     public class UnitTestForStudent
     {
         [TestMethod]
-        public void TestMethod()
+        public void TestCtor()
         {
+            Student student = new Student(0, "moophy", new DateTime(), "some street");
+            Assert.AreEqual(0, student.Id);
+            Assert.AreEqual("moophy", student.Name);
+            Assert.AreEqual(new DateTime(), student.BirthDate);
+            Assert.AreEqual("some street", student.Address);
         }
     }
 }
