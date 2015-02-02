1adae464c6712be4117ca3652f942999e0aa7b71
Mooophy <moophyalan@gmail.com>
2015-02-02 14:10:17 +1300

    new file:   Assignment5/UnitTest/UnitTestForEnrollment.cs

---
+++ Assignment5/UnitTest/UnitTestForEnrollment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEnd;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestForEnrollment
+    {
+        [TestMethod]
+        public void TestCtorById()
+        {
+            var e = new BackEnd.Enrollment<Int64>(158212, 1234567890);
+            Assert.AreEqual(158212, e.PaperCode);
+            Assert.AreEqual(1234567890, e.StudentId);
+        }
+
+        [TestMethod]
+        public void TestCtorByInstance()
+        {
+            var student = new BackEnd.Student<Int64>(1234567890, "zz", new DateTime(), "China");
+            var AppDev = new BackEnd.Paper<Int64>(158212, "App Dev", "some cute guy");
+            var e = new BackEnd.Enrollment<Int64>(AppDev, student);
+
+            Assert.AreEqual(158212, e.PaperCode);
+            Assert.AreEqual(1234567890, e.StudentId);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var e = new BackEnd.Enrollment<Int64>(158212, 1234567890);
+            Assert.AreEqual("158212,1234567890", e.ToString());
+            
+        }
+
+    }
+}
