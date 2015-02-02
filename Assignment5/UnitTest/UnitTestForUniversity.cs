f4cca024413410713a0d3236e818185346a11464
Mooophy <moophyalan@gmail.com>
2015-02-03 03:29:20 +1300

    a few UT

--- Assignment5/UnitTest/UnitTestForUniversity.cs
+++ Assignment5/UnitTest/UnitTestForUniversity.cs
@@ -60,5 +60,40 @@ namespace UnitTest
 
             Assert.AreEqual(100, uni.Students.Count);
         }
+
+        [TestMethod]
+        public void TestMethodAddRangePappers()
+        {
+            var uni = new BackEnd.University();
+            var papers = from code in Enumerable.Range(159000, 1000)
+                         select (new BackEnd.Paper<long>(code, "paper " + code.ToString(),"Lee"));
+            uni.AddRange(papers);
+
+            Assert.AreEqual(1000, uni.Papers.Count);
+        }
+
+        [TestMethod]
+        public void TestMethodFindPaper()
+        {
+            var uni = new BackEnd.University();
+            var papers = from code in Enumerable.Range(159000, 1000)
+                         select (new BackEnd.Paper<long>(code, "paper " + code.ToString(), "Lee"));
+            uni.AddRange(papers);
+
+            Assert.AreEqual("Lee", uni.FindPaper(159050).Coordinator);
+            Assert.AreEqual("paper 159050", uni.FindPaper(159050).Name);
+        }
+
+        [TestMethod]
+        public void TestMethodFindStudent()
+        {
+            var uni = new BackEnd.University();
+            var students = from id in Enumerable.Range(120000, 100)
+                           select (new BackEnd.Student<long>(id, "some guy " + id.ToString(), DateTime.Now, "some where"));
+            uni.AddRange(students);
+
+            Assert.AreEqual(120000, uni.FindStudent(120000).Id);
+            Assert.AreEqual("some guy 120050", uni.FindStudent(120050).Name);
+        }
     }
 }
