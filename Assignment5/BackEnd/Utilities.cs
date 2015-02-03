898087540f9f3c29e2bf0e62428797854e798e06
Mooophy <moophyalan@gmail.com>
2015-02-03 16:36:49 +1300

    new file:   Assignment5/BackEnd/Utilities.cs

---
+++ Assignment5/BackEnd/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BackEnd
+{
+    public class Utilities
+    {
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            using (var reader = File.OpenText(file))
+                for (string line; (line = reader.ReadLine()) != null;/* */)
+                    yield return line;
+        }
+    }
+}
