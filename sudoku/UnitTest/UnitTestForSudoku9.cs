307eac4790146e035aded0c6bc77acad665f6e83
moophy <MoophyAlan@gmail.com>
2015-02-13 00:31:27 +1300

    new file:   sudoku/UnitTest/UnitTestForSudoku9.cs

---
+++ sudoku/UnitTest/UnitTestForSudoku9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Library;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestForSudoku9
+    {
+        [TestMethod]
+        public void TestRangSet9()
+        {
+            var set = new Sudoku9.RangSet9();
+
+            Assert.AreEqual(3, set.Count);
+            Assert.AreEqual(true, set.Any(r => r.Begin == 6));
+            Assert.AreEqual(true, set.All(r => r.Count == 3));
+        }
+
+        [TestMethod]
+        public void TestCtor()
+        {
+            var s9 = new Library.Sudoku9();
+
+            Assert.AreEqual(9, s9.Size);
+            Assert.AreEqual(81, s9.Data.Length);
+            Assert.AreEqual(3, s9.ColRangeSet.Count);
+            Assert.AreEqual(3, s9.RowRangeSet.Count);
+        }
+    }
+}
