ec87362f4f666fc4c4d90200ea1451d92d691766
moophy <MoophyAlan@gmail.com>
2015-02-13 18:28:00 +1300

    passed a few UT

--- sudoku/UnitTest/UnitTestForSudoku4.cs
+++ sudoku/UnitTest/UnitTestForSudoku4.cs
@@ -41,5 +41,52 @@ namespace UnitTest
             Assert.AreEqual(0, s4.Data[3, 3]);
             Assert.AreEqual(16, s4.Data.Length);
         }
+
+        [TestMethod]
+        public void TestCtorWithData()
+        {
+            var s4 = new Library.Sudoku4(_Solved);
+
+            Assert.AreEqual(4, s4.Size);
+            Assert.AreEqual(2, s4.RowRangeSet.Count);
+            Assert.AreEqual(2, s4.ColRangeSet.Count);
+
+            Assert.AreEqual(3, s4.Data[0, 0]);
+            Assert.AreEqual(2, s4.Data[3, 3]);
+            Assert.AreEqual(16, s4.Data.Length);
+        }
+
+        [TestMethod]
+        public void TestFindRowRange()
+        {
+            var s4 = new Library.Sudoku4(_Solved);
+            var expected = new Library.Matrix.Range(0, 2);
+            var actual = s4.FindRowRange(1);
+
+            Assert.AreEqual(expected.Begin, actual.Begin);
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [TestMethod]
+        public void TestFindColRange()
+        {
+            var s4 = new Library.Sudoku4(_Solved);
+            var expected = new Library.Matrix.Range(2, 2);
+            var actual = s4.FindColRange(3);
+
+            Assert.AreEqual(expected.Begin, actual.Begin);
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [TestMethod]
+        public void TestCheck()
+        {
+            var good = new int[4] { 1, 2, 3, 4 };
+            var bad = new int[4] { 2, 2, 3, 4 };
+            var s4 = new Library.Sudoku4();
+
+            Assert.AreEqual(true, s4.Check(good));
+            Assert.AreEqual(false, s4.Check(bad));
+        }
     }
 }
