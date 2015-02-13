a6821896c46ca219e0ea0be773e609eb148417e2
moophy <MoophyAlan@gmail.com>
2015-02-13 21:09:06 +1300

    new file:   sudoku/Library/Sudoku6.cs 	modified:   sudoku/UnitTest/UnitTest.csproj 	new file:   sudoku/UnitTest/UnitTestForSudoku6.cs

---
+++ sudoku/Library/Sudoku6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Sudoku6
+    {
+        public class RowRangeSet : HashSet<Matrix.Range>
+        {
+            public RowRangeSet()
+            {
+                this.Add(new Matrix.Range(0, 2));
+                this.Add(new Matrix.Range(2, 2));
+                this.Add(new Matrix.Range(4, 2));
+            }
+        }
+
+        public class ColRangeSet : HashSet<Matrix.Range>
+        {
+            public ColRangeSet()
+            {
+                this.Add(new Matrix.Range(0, 3));
+                this.Add(new Matrix.Range(3, 3));
+            }
+        }
+    }
+}
---
+++ sudoku/UnitTest/UnitTestForSudoku6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestForSudoku6
+    {
+        int[,] _Solved = new int[,]
+        {
+            {2,4,6,5,3,1},
+            {5,1,3,6,2,4},
+            {1,6,2,3,4,5},
+            {3,5,4,2,1,6},
+            {6,2,1,4,5,3},
+            {4,3,5,1,6,2}
+        };
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
