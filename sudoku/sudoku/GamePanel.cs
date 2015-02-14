15de813022f68171dce7d9f16c6c82528737a5cb
moophy <MoophyAlan@gmail.com>
2015-02-14 20:16:35 +1300

    fix bug : #8

--- sudoku/sudoku/GamePanel.cs
+++ sudoku/sudoku/GamePanel.cs
@@ -19,13 +19,12 @@ namespace Sudoku
         public GamePanel(string path)
         {
             InitializeComponent();
-            this.Init(path);
+            this.BackEnd = new BackEnd(path);
+            this.Init();
         }
 
-        private void Init(string path)
+        private void Init()
         {
-            this.BackEnd = new BackEnd(path);
-
             int buttonSize = 80;
             this.Size = new Size((buttonSize + 4) * this.BackEnd.Matrix.Count, 
                                  (buttonSize + 8) * this.BackEnd.Matrix.Count);
@@ -36,8 +35,10 @@ namespace Sudoku
                 foreach (var col in Enumerable.Range(0, this.BackEnd.Matrix.Count))
                 {
                     var sbutton = new SButton(buttonSize, this.BackEnd.Matrix.Count, row, col);
+
                     sbutton.Location = new Point(col * buttonSize, row * buttonSize);
                     sbutton.MouseWheel += this.SetValue;
+                    sbutton.Text = this.BackEnd.Matrix.Data[row, col].ToString();
                     Board[row, col] = sbutton;
                     this.Controls.Add(Board[row, col]);
                 }
