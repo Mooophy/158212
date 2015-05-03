06234fd641f6c47902560ffa7acbb80340c0962a
moophy <MoophyAlan@gmail.com>
2015-05-04 11:57:25 +1200

    first commit

---
+++ 2015s1a4/TextAnalysis/Library/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Functions
+    {
+        static public Dictionary<string, int> MakeLengthsDic(List<string> list)
+        {
+            return list
+                .GroupBy(s => s)
+                .ToDictionary(g => g.Key, g => g.Count());
+        }
+    }
+
+}
---
+++ 2015s1a4/TextAnalysis/UnitTests/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Library;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMakeLengthsDic()
+        {
+            var seq = new List<string> { "a", "b", "c", "a", "c", "zzz", "zzz" };
+            var dic = Library.Functions.MakeLengthsDic(seq);
+
+            Assert.AreEqual(2, dic["a"]);
+            Assert.AreEqual(1, dic["b"]);
+            Assert.AreEqual(2, dic["zzz"]);
+            Assert.AreEqual(4, dic.Count);
+        }
+    }
+}
